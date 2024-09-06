ba905f82956290923fd4e69df7cc24d8f4cfc133
AndreKuratomi <andrekuratomi@gmail.com>
2024-09-06 19:16:31 -0300

    feat: essential projects (first time approach)

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StorageLogistic.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
